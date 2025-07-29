0cc06227c9398ff6694a263ba6dce391dcc75ca7
cj <cjcorc10@gmail.com>
2025-07-29 07:00:25 -0500

    centered bg

--- src/app/components/mainContainer.tsx
+++ src/app/components/mainContainer.tsx
@@ -4,7 +4,7 @@ type MainContainerProps = {
 
 export default function MainContainer({ children }: MainContainerProps) {
   return (
-    <main className="flex flex-col p-4 flex-1  m-2 rounded-2xl bg-[url('images/dots.jpg')] bg-cover ">
+    <main className="flex flex-col p-4 flex-1  m-2 rounded-2xl bg-[url('images/dots.jpg')] bg-cover bg-center">
       {children}
     </main>
   );
