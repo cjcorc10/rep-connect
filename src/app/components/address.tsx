0c2a5b205e30fbeec75ab108bceb2d8fe9794b24
cj <cjcorc10@gmail.com>
2025-10-02 17:16:35 -0500

    removed animation from Address component for smoother skeleton transition

--- src/app/components/address.tsx
+++ src/app/components/address.tsx
@@ -1,31 +1,11 @@
-'use client';
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { useRef } from 'react';
-
 type AddressProps = {
   address: string;
 };
 
 export default function Address({ address }: AddressProps) {
-  const addressRef = useRef<HTMLDivElement>(null);
-  useGSAP(
-    () => {
-      gsap.from(addressRef.current, {
-        scale: 0,
-        ease: 'back',
-        duration: 1,
-      });
-    },
-    { dependencies: [address] }
-  );
-
   return (
     <div className="flex justify-center items-center">
-      <div
-        className="flex justify-center items-center w-24 h-24 bg-blue-200 opacity-80 rounded-full z-0"
-        ref={addressRef}
-      >
+      <div className="flex justify-center items-center w-24 h-24 bg-blue-200 opacity-80 rounded-full z-0">
         <h1 className="text-3xl text-blue-700 z-10 font-bold whitespace-nowrap">
           {address}
         </h1>
