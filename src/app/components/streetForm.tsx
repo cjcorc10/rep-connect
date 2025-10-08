e2bca74858e1bca3f7f14bb5914780068ad70cc0
cj <cjcorc10@gmail.com>
2025-10-07 19:16:44 -0500

    moved all validation to client to improve performance

--- src/app/components/streetForm.tsx
+++ src/app/components/streetForm.tsx
@@ -2,24 +2,40 @@
 import { Search } from 'lucide-react';
 import Button from './button';
 import { useParams } from 'next/navigation';
-import { ErrorState, validateStreetAddress } from '../lib/actions';
-import { useActionState } from 'react';
-
-const initialState: ErrorState = {
-  error: undefined,
-  message: undefined,
-};
+import { useState } from 'react';
+import z from 'zod';
+import { useRouter } from 'next/navigation';
 
 export default function StreetForm() {
   const { zip } = useParams();
-  const [state, formAction] = useActionState(
-    validateStreetAddress,
-    initialState
-  );
+  const router = useRouter();
+
+  const [error, setError] = useState<string | null>(null);
+  const StreetSchema = z.object({
+    street: z.string().min(1),
+    zip: z.string().min(5),
+  });
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const parsedData = StreetSchema.safeParse({
+      street: formData.get('street'),
+      zip: formData.get('zip'),
+    });
+
+    if (!parsedData.success) {
+      setError('Please enter a valid street address.');
+      return;
+    }
+    const { street, zip } = parsedData.data;
+
+    router.push(`/reps/${zip}?street=${encodeURIComponent(street)}`);
+  };
 
   return (
     <form
-      action={formAction}
+      onSubmit={onSubmit}
       className="flex bg-white rounded-2xl shadow-md px-4 py-2 mt-4 max-w-full"
     >
       <input type="hidden" name="zip" value={zip} />
