fa57be17a0bee7ed5b47a4d7effa8c46d68ce22e
cj <cjcorc10@gmail.com>
2026-01-12 20:37:53 -0600

    Refactor RepCard component for improved styling and layout. Removed unused CSS, updated class names, and streamlined the structure for better readability and performance.

--- src/app/components/repCard/repCard.tsx
+++ src/app/components/repCard/repCard.tsx
@@ -23,12 +23,10 @@ export default function RepCard({ rep }: RepCardProp) {
       layoutId={`rep-card-${rep.bioguide_id}`}
       onClick={() => setSelectedRep(rep)}
       className={clsx(
-        styles.repCard,
-
         rep.party === "Republican"
-          ? "border-l-red-500 border-l-4"
-          : "border-l-blue-500 border-l-4",
-        "relative rounded-xl flex flex-row shadow-lg hover:bg-gray-50 cursor-pointer overflow-hidden h-[65vh]"
+          ? "border-l-red-500"
+          : "border-l-blue-500",
+        "relative flex flex-row cursor-pointer h-[65vh] rounded-xl overflow-hidden border-l-4"
       )}
       whileTap={{ scale: 0.98 }}
       transition={{
@@ -39,7 +37,10 @@ export default function RepCard({ rep }: RepCardProp) {
     >
       <motion.div
         layoutId={`rep-image-${rep.bioguide_id}`}
-        className="absolute w-full h-full overflow-hidden shadow-md"
+        className={clsx(
+          styles.repCardImage,
+          "absolute w-full h-full"
+        )}
       >
         <Image
           src={imageUrl || rep.image_url || ""}
@@ -48,22 +49,15 @@ export default function RepCard({ rep }: RepCardProp) {
           style={{ objectFit: "cover", objectPosition: "" }}
         />
       </motion.div>
-      <div className="flex flex-col justify-center">
-        <div className=" flex flex-col gap-1 z-20">
-          <motion.h3
-            layoutId={`${rep.bioguide_id}-name`}
-            className="text-2xl font-bold flex items-center text-white"
-          >
-            {rep.full_name}
-          </motion.h3>
-          <motion.p layoutId={`${rep.bioguide_id}-role`}>
-            {role}
-          </motion.p>
-          <motion.p layoutId={`${rep.bioguide_id}-party`}>
-            {rep.party}
-          </motion.p>
-        </div>
-      </div>
+      <motion.h3
+        layoutId={`${rep.bioguide_id}-name`}
+        className={clsx(
+          styles.repCardName,
+          "text-[5rem] text-white font-bold rounded-t-lg absolute bottom-0 mix-blend-difference"
+        )}
+      >
+        {rep.last_name}
+      </motion.h3>
     </motion.div>
   );
 }
