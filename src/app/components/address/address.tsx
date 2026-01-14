f518ef59afe2b0ee6b3250abc538b30efba14616
cj <cjcorc10@gmail.com>
2026-01-14 12:21:54 -0600

    added button to form in Address.tsx

--- src/app/components/address/address.tsx
+++ src/app/components/address/address.tsx
@@ -1,6 +1,8 @@
+import { Pencil, PencilIcon } from "lucide-react";
 import styles from "./address.module.css";
 import { useState, useEffect } from "react";
 export default function Address({ address }: { address: string }) {
+  const [value, setValue] = useState(address);
   const [mounted, setMounted] = useState(false);
   useEffect(() => {
     setMounted(true);
@@ -8,8 +10,9 @@ export default function Address({ address }: { address: string }) {
   return (
     <div className={styles.addressContainer}>
       <form>
-        <input className={styles.addressTitle} data-mounted={mounted} value={address}
-          onChange={(e) => address =  e.currentTarget.value}/>
+        <input className={styles.addressTitle} data-mounted={mounted} value={value}
+          onChange={(e) => setValue(e.currentTarget.value)}/>
+        <button className={styles.submit} onClick={(e) => e.stopPropagation()}><Pencil /></button>
       </form>
     </div>
   );
