ab0b8be40879e1a123f012ad5b3b1bf9d9685aab
cj <cjcorc10@gmail.com>
2026-09-16 18:55:00 -0500

    added build to ci script

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm start",
+    command: "pnpm run start",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
